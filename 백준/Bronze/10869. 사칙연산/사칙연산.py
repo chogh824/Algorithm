3de93aaf7bbfbd8e5ec171b6337b924d4641cd3c
chogh824 <76734412+chogh824@users.noreply.github.com>
2023-09-08 14:18:50 +0900

    [Bronze V] Title: 사칙연산, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.py
@@ -0,0 +1,9 @@
+a,b = input().split()        #두 자연수를 입력받고 쪼개기
+a = int(a)                #문자형이 디폴트이기 때문에 int형으로 형변환시켜주어야 함
+b = int(b)
+
+print(a+b)               #더하기
+print(a-b)                #빼기
+print(a*b)                #곱하기
+print(a//b)                #나누기
+print(a%b)               #나머지
