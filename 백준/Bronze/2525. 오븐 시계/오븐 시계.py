c42d4bf75f0b9e70aa79fb2409ecae2104117283
chogh824 <76734412+chogh824@users.noreply.github.com>
2023-09-09 23:18:43 +0900

    [Bronze III] Title: 오븐 시계, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2525. 오븐 시계/오븐 시계.py
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.py
@@ -1,15 +1,13 @@
-a, b = map(int,input().split())
-c = int(input())
+a, b = map(int,input().split())            # 현재 시각 입력받음
+c = int(input())                           # 오븐 시간 입력받음
 
-sum = a * 60 + b + c
+sum = a * 60 + b + c                       # 총 시간 계산
+a = sum // 60                              # 시
+b = sum % 60                               # 분
 
-if sum >= 1440:
-    sum -= 1440
-    a = sum // 60
-    b = sum % 60
+if sum >= 1440:                            # 하루를 오버했을 때, 1440을 빼기 위한 if문
+    a -= 24                                
     print(a,b)
 
 else:
-    a = sum // 60
-    b = sum % 60
     print(a,b)
