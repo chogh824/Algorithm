979ca441b974f2b19a4dbfa18e6b8c998d3ebe75
chogh824 <76734412+chogh824@users.noreply.github.com>
2024-08-25 15:51:42 +0900

    [Bronze V] Title: A×B, Time: 100 ms, Memory: 14232 KB -BaekjoonHub

--- 백준/Bronze/10998. A×B/A×B.java
+++ 백준/Bronze/10998. A×B/A×B.java
@@ -1,8 +1,17 @@
-import java.util.*;
+import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        System.out.println(s.nextInt() * s.nextInt());
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(st.nextToken());
+        int X = Integer.parseInt(st.nextToken());
+
+        bw.write(String.valueOf(N * X));
+        bw.flush(); // 버퍼의 내용을 강제로 출력
+        bw.close(); // BufferedWriter를 닫아줌으로써 자원 해제
     }
 }
