1704940c34645deaec16c1abe5241999c0294aae
chogh824 <76734412+chogh824@users.noreply.github.com>
2024-09-04 14:43:11 +0900

    [Bronze IV] Title: 주사위 세개, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.py
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.py
@@ -1,17 +1,12 @@
 a,b,c = map(int,input().split())
 
-if a!=b and b!=c and c!=a:
-    if a>b and a>c:
-        print(a*100)
-    elif b>a and b>c:
-        print(b*100)
-    else:
-        print(c*100)
-elif a==b and a!=c:
+if a == b == c:
+    print(10000+a*1000)
+elif a == b:
     print(1000+a*100)
-elif a==c and a!=b:
+elif a == c:
     print(1000+a*100)
-elif b==c and a!=b:
+elif b == c:
     print(1000+b*100)
 else:
-    print(10000+1000*a)
+    print(100 * max(a,b,c))
