43d04eadfa73ac83d2a60a853d30a66c2a9a24d1
chogh824 <76734412+chogh824@users.noreply.github.com>
2025-03-20 11:53:01 +0900

    [Silver I] Title: 신입 사원, Time: 2776 ms, Memory: 309500 KB -BaekjoonHub

--- Java/백준/Silver/1946. 신입 사원/신입 사원.java
+++ Java/백준/Silver/1946. 신입 사원/신입 사원.java
@@ -1,26 +1,24 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Comparator;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
-        
+
         for (int i = 0; i < T; i++) {
             int N = Integer.parseInt(br.readLine());
             int[][] arr = new int[N][2];
-            
+
             for (int j = 0; j < N; j++) {
                 StringTokenizer st = new StringTokenizer(br.readLine(), " ");
                 arr[j][0] = Integer.parseInt(st.nextToken());
                 arr[j][1] = Integer.parseInt(st.nextToken());
             }
 
-            Arrays.sort(arr, Comparator.comparingInt(a -> a[0]));
+            mergeSort(arr, 0, N - 1);
 
             int count = 1;
             int minRank = arr[0][1];
@@ -35,4 +33,34 @@ public class Main {
             System.out.println(count);
         }
     }
+
+    private static void mergeSort(int[][] arr, int left, int right) {
+        if (left < right) {
+            int mid = (left + right) / 2;
+            mergeSort(arr, left, mid);
+            mergeSort(arr, mid + 1, right);
+            merge(arr, left, mid, right);
+        }
+    }
+
+    private static void merge(int[][] arr, int left, int mid, int right) {
+        int[][] temp = new int[right - left + 1][2];
+        int i = left, j = mid + 1, k = 0;
+
+        while (i <= mid && j <= right) {
+            if (arr[i][0] <= arr[j][0]) {
+                temp[k++] = arr[i++];
+            } else {
+                temp[k++] = arr[j++];
+            }
+        }
+
+        while (i <= mid) temp[k++] = arr[i++];
+        while (j <= right) temp[k++] = arr[j++];
+
+        for (i = left, k = 0; i <= right; i++, k++) {
+            arr[i] = temp[k];
+        }
+    }
 }
+
